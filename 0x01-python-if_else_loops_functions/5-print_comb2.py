a7fe208b23f692922e67cfedb250acfc86f67e91
jarehec <bone1198@gmail.com>
2017-05-02 21:41:43 +0000

    using print formmating to add zeros

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 for i in range(99):
-#    if i < 10:
- #       print("0", end='')
-    if i < 100:
-        print("{:d}".format(i).zfill(2) + ", ", end='')
+        print("{:02d}, ".format(i), end='')
 print("{:d}".format(99))
