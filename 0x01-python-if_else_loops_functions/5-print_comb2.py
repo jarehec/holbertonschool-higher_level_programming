bc4027fadf844159cfdb980fd410623ec87240e2
jarehec <bone1198@gmail.com>
2017-05-02 21:35:11 +0000

    using zfill

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for i in range(99):
-    if i < 10:
-        print("0", end='')
-    print("{:d}, ".format(i), end='')
+#    if i < 10:
+ #       print("0", end='')
+    if i < 100:
+        print("{:d}".format(i).zfill(2) + ", ", end='')
 print("{:d}".format(99))
