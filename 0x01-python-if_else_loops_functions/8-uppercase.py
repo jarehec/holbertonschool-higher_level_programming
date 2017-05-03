4049c426fa16835287de05a72a184d7105f6510d
jarehec <bone1198@gmail.com>
2017-05-03 05:15:42 +0000

    removed extra print

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,6 @@
 def uppercase(str):
     for c in str:
         if ord(c) > 96:
-            print("{:s}".format(chr(ord(c) - 32)), end='')
-        else:
-            print("{:s}".format(chr(ord(c))), end='')
+            c = chr(ord(c) - 32)
+        print("{:s}".format(chr(ord(c))), end='')
     print("")
