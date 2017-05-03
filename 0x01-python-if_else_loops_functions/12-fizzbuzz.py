4e827efd14334eb1c2a532bc3279f6edde6e8c84
jarehec <bone1198@gmail.com>
2017-05-03 00:23:05 +0000

    initial commit

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(101):
+        if i == 0:
+            continue
+        if i % 15 == 0:
+            print("FizzBuzz", end='')
+        elif i % 5 == 0:
+            print("Buzz", end='')
+        elif i % 3 == 0:
+            print("Fizz", end='')
+        else:
+            print(i, end='')
+        if i < 101:
+            print(" ", end='')
