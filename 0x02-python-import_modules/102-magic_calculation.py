e2cc8c7ea911266b69a5619330545d4fdf587c5f
jarehec <bone1198@gmail.com>
2017-05-04 23:30:42 +0000

    matched to example output

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -6,6 +6,6 @@ def magic_calculation(a, b):
         for i in range(4, 6):
             c = add(c, i)
         return c
-    add(a, b)
-    sub(a, b)
-    return
+    else:
+        return sub(a, b)
+    return 0
