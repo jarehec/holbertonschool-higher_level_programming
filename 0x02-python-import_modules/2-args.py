de5a103d3580959604a36e3f2ab4a281f59c3255
jarehec <bone1198@gmail.com>
2017-05-04 19:28:16 +0000

    removed extra print statements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,11 @@ if __name__ == '__main__':
     ac = len(sys.argv) - 1
     s = "argument"
     if ac < 1:
-        print("{:d} {:s}.".format(ac, s))
-    elif ac > 0:
-        if ac > 2:
-            s += 's'
-        print("{:d} {:s}:".format(ac, s))
+        s += '.'
+    elif ac == 1:
+        s += ':'
+    elif ac > 1:
+            s += 's:'
+    print("{:d} {:s}".format(ac, s))
     for av in range(1, ac + 1):
         print("{:d}: {:s}".format(av, sys.argv[av]))
