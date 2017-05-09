5f3dfd2a9eb94566184834de40d28d5a9d416bb5
jarehec <bone1198@gmail.com>
2017-05-09 22:47:08 +0000

    fixed style

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,5 @@ def multiple_returns(sentence):
     if len(sentence) < 0:
         tpl = (0, None)
     elif len(sentence) > 0:
-       tpl = (len(sentence), sentence[0]) 
+        tpl = (len(sentence), sentence[0])
     return (tpl)
