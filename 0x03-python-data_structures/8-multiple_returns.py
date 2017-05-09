d36c1c247a18c16ab409c14609d3bdcec41d5b6b
jarehec <bone1198@gmail.com>
2017-05-09 22:48:22 +0000

    accounting for zero

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) < 0:
+    if len(sentence) < 1:
         tpl = (0, None)
     elif len(sentence) > 0:
         tpl = (len(sentence), sentence[0])
