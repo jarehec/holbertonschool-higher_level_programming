6b17b8457b234f1053e6cc26820371c28064aa40
jarehec <bone1198@gmail.com>
2017-05-09 21:55:48 +0000

    revert

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for lists in matrix:
-        str_a = []
         for i in lists:
-            str_a.append(str(i))
-        print(" ".join(str_a))
+            print("{:d}".format(i), end='')
+            if i % 3 != 0:
+                print(' ', end='')
+        print("")
