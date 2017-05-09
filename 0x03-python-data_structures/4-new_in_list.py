d973e9113aa0c8e260610a86eb4cefc04cf0d77d
jarehec <bone1198@gmail.com>
2017-05-09 21:07:52 +0000

    check for negative

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = my_list + []
-    if idx >= len(my_list):
+    if idx >= len(my_list) or idx < 0:
         return new_list
     new_list[idx] = element
     return new_list
