c82da911a50584c4a915329c7288aa5ee92a43b7
jarehec <bone1198@gmail.com>
2017-05-09 22:45:37 +0000

    initial commit

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) < 0:
+        tpl = (0, None)
+    elif len(sentence) > 0:
+       tpl = (len(sentence), sentence[0]) 
+    return (tpl)
