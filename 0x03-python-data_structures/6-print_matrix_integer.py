c5a259e4a314504f36a3c9ea004a7ad730fb2693
jarehec <bone1198@gmail.com>
2017-05-10 06:13:04 +0000

    check for None

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for lists in matrix:
-        for i in lists:
-            print("{:d}".format(i), end='')
-            if i % len(lists) != 0:
-                print(' ', end='')
-        print("")
+    if matrix is not None:
+        for lists in matrix:
+            for i in lists:
+                print("{:d}".format(i), end='')
+                if i % len(lists) != 0:
+                    print(' ', end='')
+            print("")
