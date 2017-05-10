04f33b99543f809e8ee55add315fad02d4da2efe
jarehec <bone1198@gmail.com>
2017-05-10 20:44:48 +0000

    check idx for negative

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if my_list[idx] is not None:
+    if my_list is not None and len(my_list) > 0:
+        if idx < 0:
+            return my_list
         del my_list[idx]
         return my_list
