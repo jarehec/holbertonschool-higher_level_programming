e971aa3b97f29e77ca34bf05be5f2b36e67bb0df
jarehec <bone1198@gmail.com>
2017-05-10 00:43:05 +0000

    using reverse instead of sort

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if len(my_list) > 0:
-        my_list.sort(reverse=True)
+        my_list.reverse()
         for item in (my_list):
             print("{:d}".format(item))
