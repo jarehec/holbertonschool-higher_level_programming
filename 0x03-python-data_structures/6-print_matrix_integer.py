811422a17a8055a68812c93476ef5eae507d230e
jarehec <bone1198@gmail.com>
2017-05-10 20:13:19 +0000

    change and to or

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is not None and matrix != []:
+    if matrix is not None or matrix != []:
         for lists in matrix:
             for i in lists:
                 print("{:d}".format(i), end='')
