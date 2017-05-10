9dd2fa7a5bdacfb9766788a418b73240e04c52d9
jarehec <bone1198@gmail.com>
2017-05-10 20:48:53 +0000

    check for empty list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if my_list is not None and len(my_list) > 0:
+    if my_list is not None:
         if idx < 0 or idx > len(my_list) - 1:
             return my_list
         del my_list[idx]
