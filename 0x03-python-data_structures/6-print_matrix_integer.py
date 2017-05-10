0436c2d92973ce3432fff2d12f10bc0a98313b9b
jarehec <bone1198@gmail.com>
2017-05-10 19:56:07 +0000

    check matrix length

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is not None:
+    if matrix is not None or len(matrix) != 0:
         for lists in matrix:
             for i in lists:
                 print("{:d}".format(i), end='')
