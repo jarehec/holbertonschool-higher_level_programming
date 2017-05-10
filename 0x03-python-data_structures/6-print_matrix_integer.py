c98ff82521f0d2c301dfab221950133d35e6a06a
jarehec <bone1198@gmail.com>
2017-05-10 20:31:43 +0000

    adding spaces if len is not divible by the element count

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix is not None or matrix != []:
+        c = 1
         for lists in matrix:
             for i in lists:
                 print("{:d}".format(i), end='')
-                if i % len(lists) != 0:
+                if c % len(lists) != 0:
+                    c += 1
                     print(' ', end='')
+                else:
+                    c = 1
             print("")
