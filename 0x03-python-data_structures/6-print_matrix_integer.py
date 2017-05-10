ccd6a853c704bcd2510536817116c31234e1b208
jarehec <bone1198@gmail.com>
2017-05-10 06:10:34 +0000

    using the length of list to add spaces

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for lists in matrix:
         for i in lists:
             print("{:d}".format(i), end='')
-            if i % 3 != 0:
+            if i % len(lists) != 0:
                 print(' ', end='')
         print("")
