81e957155def70854e1b742e103fb96842f40e1b
jarehec <bone1198@gmail.com>
2017-05-10 01:01:40 +0000

    not using builtin function

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     if len(my_list) > 0:
         my_list.reverse()
-        for item in (my_list):
-            print("{:d}".format(item))
+        for item in range(len(my_list)):
+            print("{:d}".format(my_list[item]))
