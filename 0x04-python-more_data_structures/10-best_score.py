29fe8ebafc6b072a9e9e0ff10f49186c8b77a1f8
jarehec <bone1198@gmail.com>
2017-05-11 23:48:36 +0000

    check my_dict length for 0

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(my_dict):
-    if my_dict is not None:
+    if my_dict is not None and len(my_dict) > 0:
         big = 0
         for key, val in my_dict.items():
             if val >= big:
