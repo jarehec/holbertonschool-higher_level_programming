d7e65e544bff3e5ce32ce302e3f647f2ced86b5d
jarehec <bone1198@gmail.com>
2017-05-11 23:44:06 +0000

    returning none if no score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,9 +2,10 @@
 def best_score(my_dict):
     if my_dict is not None:
         big = 0
-        b_val = ''
         for key, val in my_dict.items():
             if val > big:
                 big = val
                 b_val = key
         return b_val
+    else:
+        return None
