088caa4d4f76322880328a6e06ea34b153bef132
jarehec <bone1198@gmail.com>
2017-05-11 23:46:11 +0000

    >=

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,9 +3,7 @@ def best_score(my_dict):
     if my_dict is not None:
         big = 0
         for key, val in my_dict.items():
-            if val > big:
+            if val >= big:
                 big = val
                 b_val = key
         return b_val
-    else:
-        return None
