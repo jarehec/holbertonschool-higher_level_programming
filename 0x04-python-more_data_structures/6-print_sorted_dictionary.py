8825ed93b705d4ef050e37441cd543dc40e6067a
jarehec <bone1198@gmail.com>
2017-05-11 22:09:14 +0000

    fixed sorting order

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(my_dict):
-    if my_dict is not None:
-        keys = list(sorted(my_dict.keys()))
-        val = list(my_dict.values())
-        for i in range(len(keys)):
-            print("{:s}: {}".format(keys[i], val[i]))
+    for keys, val in sorted(my_dict.items()):
+        print("{:s}: {}".format(keys, val))
