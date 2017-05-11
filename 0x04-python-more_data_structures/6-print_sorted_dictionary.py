e4b709b3166c51f1ae94e72583f0fb3a710e7f24
jarehec <bone1198@gmail.com>
2017-05-11 21:58:49 +0000

    check for none

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(my_dict):
-    keys = list(sorted(my_dict.keys()))
-    val = list(my_dict.values())
-    for i in range(len(keys)):
-        print("{:s}: {}".format(keys[i], val[i]))
+    if my_dict is not None:
+        keys = list(sorted(my_dict.keys()))
+        val = list(my_dict.values())
+        for i in range(len(keys)):
+            print("{:s}: {}".format(keys[i], val[i]))
