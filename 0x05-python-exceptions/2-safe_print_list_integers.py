636404ee3bae9e6e9f92b97950a6f2ce8c93aee3
jarehec <bone1198@gmail.com>
2017-05-12 18:56:29 +0000

    decremenet i in except

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,6 +6,6 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[ele]), end='')
         except (ValueError, TypeError):
-            pass
+            i -= 1
     print("")
     return i
