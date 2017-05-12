dba3906c9cf781d6a072ed4506d14c553d22769f
jarehec <bone1198@gmail.com>
2017-05-12 21:37:13 +0000

    forgot to add finally

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,4 +13,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             new.append(0)
             print("out of range")
+        finally:
+            pass
     return new
