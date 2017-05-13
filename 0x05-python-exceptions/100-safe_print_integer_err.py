441c1781715e5974dc4681ff6113438ea103fbec
jarehec <bone1198@gmail.com>
2017-05-13 04:46:19 +0000

    initial commit

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as e:
+        sys.stderr.write("Exception: ")
+        sys.stderr.write(str(e))
+        sys.stderr.write('\n')
+        return False
