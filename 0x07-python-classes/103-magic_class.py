2a5dda67e7eeb4747af43f6f0b1d721ea019ad9e
jarehec <bone1198@gmail.com>
2017-05-23 06:41:00 +0000

    removed dis.dis

--- 0x07-python-classes/103-magic_class.py
+++ 0x07-python-classes/103-magic_class.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-import dis
+"""Module for magic"""
 import math
 
 
-
-
 class MagicClass:
+    'magic class'
     def __init__(self, radius):
+        'initialization'
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
-        return 
 
     def area(self):
+        'area method'
         return self.__radius**2 * math.pi
 
-
     def circumference(self):
+        'circumference method'
         return 2 * math.pi * self.__radius
-
-dis.dis(MagicClass)
