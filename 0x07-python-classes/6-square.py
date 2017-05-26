6dade5e9bb3088b86975b86f7207e6760047f54e
jarehec <bone1198@gmail.com>
2017-05-26 06:13:19 +0000

    check setter for errors

--- 0x07-python-classes/6-square.py
+++ 0x07-python-classes/6-square.py
@@ -19,6 +19,10 @@ class Square:
     @size.setter
     def size(self, value):
         'Method to set self.__size'
+        if isinstance(value, int) is not True:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
