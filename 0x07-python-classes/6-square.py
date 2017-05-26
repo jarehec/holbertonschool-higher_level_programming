1e83b20c8afbcbae30bd6bd23fe8650be33e2e42
jarehec <bone1198@gmail.com>
2017-05-26 06:54:33 +0000

    changed loop

--- 0x07-python-classes/6-square.py
+++ 0x07-python-classes/6-square.py
@@ -57,5 +57,8 @@ class Square:
 #        	print('' * self.__position[1])
         if self.__size == 0:
             print("")
-        for i in range(self.__size):
-            print(" "* self.__position[0] +("#" * self.__size))
+        else:
+            print('' * self.__position[1])
+            for i in range(self.__size):
+                print(" " * self.__position[0], end='')
+                print("#" * self.__size)
