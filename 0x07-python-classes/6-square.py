6956d9432598fffaae21eaa560658f04899cf1e0
jarehec <bone1198@gmail.com>
2017-05-26 06:58:41 +0000

    revert

--- 0x07-python-classes/6-square.py
+++ 0x07-python-classes/6-square.py
@@ -57,7 +57,8 @@ class Square:
 #        	print('' * self.__position[1])
         if self.__size == 0:
             print("")
-        print('\n' * self.__position[1], end='')
+        else:
+            print('\n' * self.__position[1], end='')
         for i in range(self.__size):
             print(" " * self.__position[0], end='')
             print("#" * self.__size)
