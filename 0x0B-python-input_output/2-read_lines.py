633673289ea23bc8e5799c8fee256d10329a7b66
jarehec <bone1198@gmail.com>
2017-06-03 05:18:40 +0000

    changed > to >=

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -2,7 +2,7 @@
 def read_lines(filename="", nb_lines=0):
     'reads nb_lines of a text file'
     with open(filename, 'r', encoding='utf-8') as fname:
-        if nb_lines < 1 or nb_lines > len(list(fname)):
+        if nb_lines < 1 or nb_lines >= len(list(fname)):
             print(fname.read(), end='')
         else:
             fname.seek(0)
