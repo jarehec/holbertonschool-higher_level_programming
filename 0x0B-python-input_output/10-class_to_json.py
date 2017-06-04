afc3737ceb0b2b73ea445eee4704858a238b4b78
jarehec <bone1198@gmail.com>
2017-06-04 05:57:18 +0000

    revert

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def class_to_json(obj):
     'returns a dictionary description with simple data structure'
-    d = {'__dict__' : type(obj).__name__}
-    d.update(vars(obj))
-    return d
+    return vars(obj)
