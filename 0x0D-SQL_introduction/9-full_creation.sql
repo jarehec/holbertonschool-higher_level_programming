10e4222f6cd4716d8b25865e4d150d3e2f26158b
Jared HECK <129@p4-2.hbtn.io>
2017-07-13 12:18:23 -0700

    initial commit

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- creates second_table for hbtn_0c_0
+CREATE TABLE IF EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO `second_table` (`id`,`name`, score) VALUES ("1", "John", "10");
+INSERT INTO `second_table` (`id`,`name`, score) VALUES ("2", "Alex", "3");
+INSERT INTO `second_table` (`id`,`name`, score) VALUES ("3", "Bob", "14");
+INSERT INTO `second_table` (`id`,`name`, score) VALUES ("4", "George", "8");
