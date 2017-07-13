75ee647ab8aa92540e8838ef6f4009366889de30
jarehec <bone1198@gmail.com>
2017-07-13 18:13:59 +0000

    initial commit

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- lists all rows from first_table
+SELECT * FROM first_table;
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- counts the number of records in first_table with id=89
+SELECT COUNT(*)  FROM first_table WHERE id="89";
