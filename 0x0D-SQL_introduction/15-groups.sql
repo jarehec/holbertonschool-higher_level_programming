b047eaf49c8650518723f6736bab8aac3e68fbba
Jared HECK <129@p4-2.hbtn.io>
2017-07-13 14:37:34 -0700

    fixed command

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lists the number of records with the same score
-SELECT score, COUNT(*) number FROM second_table GROUP BY score HAVING number > 1 ORDER BY score DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY score DESC
