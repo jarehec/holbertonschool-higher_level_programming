0c95311641db273c5f6f76762ee6765b02f644f3
Jared HECK <129@p4-2.hbtn.io>
2017-07-13 15:31:08 -0700

    check for ''

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all records of second_table where name is not null
-SELECT name,score FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT name,score FROM second_table WHERE name IS NOT NULL name IS NOT '' ORDER BY score DESC;
