737455f52747306034883b8a52d27047eb92c393
Jared HECK <129@p4-2.hbtn.io>
2017-07-13 12:21:30 -0700

    added NOT

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- creates second_table for hbtn_0c_0
-CREATE TABLE IF EXISTS second_table (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO `second_table` (`id`,`name`, score) VALUES ("1", "John", "10");
 INSERT INTO `second_table` (`id`,`name`, score) VALUES ("2", "Alex", "3");
 INSERT INTO `second_table` (`id`,`name`, score) VALUES ("3", "Bob", "14");
