34dc99cbb3d500c0f2d1753b22aa4a487e95eaa3
jarehec <bone1198@gmail.com>
2017-07-14 21:42:46 +0000

    initial commit

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,3 @@
+-- converts hbtn_0c_0 to utf8
+ALTER DATABASE hbtn_0c_0  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utfmb4_unicode_ci;
