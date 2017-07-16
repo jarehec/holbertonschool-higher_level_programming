e85115ee2a18d8ccbab5074192801c47a046bd66
jarehec <bone1198@gmail.com>
2017-07-16 23:33:02 +0000

    initial commit

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- creates hbtn_0d_2 database and user_0d_2 user
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
