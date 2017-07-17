aeea65decbe825d6400bb4b48119c498f906a2b0
jarehec <bone1198@gmail.com>
2017-07-17 22:41:34 +0000

    removed right join

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 --  lists all shows, and all genres linked to that show in the database hbtn_0d_tvshows
 SELECT tv_shows.title, tv_genres.name
 FROM tv_genres
-RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title, tv_genres.name ASC;
