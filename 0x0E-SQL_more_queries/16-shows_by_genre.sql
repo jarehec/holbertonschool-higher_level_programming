f3bcf26e1dbce4a04daa49cf920e32f8bd5b3460
jarehec <bone1198@gmail.com>
2017-07-17 22:40:51 +0000

    fixed typo

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,5 @@
 --  lists all shows, and all genres linked to that show in the database hbtn_0d_tvshows
-SELECT tv_shows.title, tv_genre.name
+SELECT tv_shows.title, tv_genres.name
 FROM tv_genres
 RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
