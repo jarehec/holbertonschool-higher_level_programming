2b2dfa1fef7afe9fc73f77624cc87c2bac0a2ff2
jarehec <bone1198@gmail.com>
2017-07-17 02:30:08 +0000

    use database

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,4 @@
 -- creates the databse hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL, UNIQUE (id));
