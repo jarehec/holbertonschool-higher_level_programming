8442552bb739d1a6d3b3e4ebd0fc0b2f46b7dcc5
jarehec <bone1198@gmail.com>
2017-07-17 22:17:50 +0000

    order by genre name

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,4 +3,4 @@ SELECT tv_shows.title
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-ORDER BY tv_shows.title ASC;
+ORDER BY tv_shows.title, tv_genres.name ASC;
