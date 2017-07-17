eddf65ef4ac2a490d5189fe19d51054f329097fd
jarehec <bone1198@gmail.com>
2017-07-17 22:19:37 +0000

    include genre name

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,5 @@
 --  lists all shows, and all genres linked to that show in the database hbtn_0d_tvshows
-SELECT tv_shows.title
+SELECT tv_shows.title, tv_genre.name
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
