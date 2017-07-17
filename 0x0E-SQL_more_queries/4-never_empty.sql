26b3cc847e8ad0d6f97ee4d4bf066477dfb40d30
jarehec <bone1198@gmail.com>
2017-07-17 00:16:22 +0000

    initial commit

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- creates the table id_not_null
-CREATE TABLE IF NOT EXISTS id_not_null (id INT NOT NULL, name VARCHAR(256), PRIMARY KEY (id));
+CREATE TABLE IF NOT EXISTS id_not_null (id INT NOT NULL DEFAULT '1', name VARCHAR(256));
