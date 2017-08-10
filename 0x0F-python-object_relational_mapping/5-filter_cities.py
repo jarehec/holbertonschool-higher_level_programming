f92730ee18ac2f01ee68bea72e8948e6c52c99d2
jarehec <bone1198@gmail.com>
2017-08-10 21:24:29 +0000

    reduced line count

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""takes in an argument and displays all values in the
-   states table of hbtn_0e_0_usa where name matches the argument
+"""takes in the name of a state as an argument and lists all cities of that
+   states using the hbtn_0e_4_usa database
 """
 if __name__ == "__main__":
     import MySQLdb
     import sys
     if len(sys.argv) == 5:
-        user = sys.argv[1]
-        passwd = sys.argv[2]
-        database = sys.argv[3]
-        match = sys.argv[4]
         s_list = []
-        db = MySQLdb.connect(host="localhost", user=user, passwd=passwd,
-                             db=database, port=3306)
+        db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3], port=3306)
         cur = db.cursor()
         cur.execute("SELECT * FROM states,cities \
                     WHERE states.id=cities.state_id \
                     ORDER BY cities.id ASC")
         table = cur.fetchall()
         for state in table:
-            if state[1] == match:
+            if state[1] == sys.argv[4]:
                 s_list.append(state[4])
         print(', '. join(s_list))
         cur.close()
