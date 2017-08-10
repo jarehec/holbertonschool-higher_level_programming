3bbee1b821de6e63cacf2c81bc16451c48679476
jarehec <bone1198@gmail.com>
2017-08-10 19:56:22 +0000

    check length of arguments

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,17 +4,18 @@
 if __name__ == "__main__":
     import MySQLdb
     import sys
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    database = sys.argv[3]
-    db = MySQLdb.connect(host="localhost", user=user, passwd=passwd,
-                         db=database, port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, state.name \
-                FROM cities, states WHERE states.id=cities.state_id \
-                ORDER BY cities.id ASC")
-    table = cur.fetchall()
-    for state in table:
-        print(state)
-    cur.close()
-    db.close()
+    if len(sys.argv) == 4:
+        user = sys.argv[1]
+        passwd = sys.argv[2]
+        database = sys.argv[3]
+        db = MySQLdb.connect(host="localhost", user=user, passwd=passwd,
+                             db=database, port=3306)
+        cur = db.cursor()
+        cur.execute("SELECT cities.id, cities.name, state.name \
+                    FROM cities, states WHERE states.id=cities.state_id \
+                    ORDER BY cities.id ASC")
+        table = cur.fetchall()
+        for state in table:
+            print(state)
+        cur.close()
+        db.close()
