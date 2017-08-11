893368f18dae2d8c6adf09913eefb27d68d6731b
jarehec <bone1198@gmail.com>
2017-08-11 22:17:39 +0000

    fixed typo

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
         db = MySQLdb.connect(host="localhost", user=user, passwd=passwd,
                              db=database, port=3306)
         cur = db.cursor()
-        cur.execute("SELECT cities.id, cities.name, state.name \
-                    FROM cities JOIN states ON states.id=cities.state_id \
+        cur.execute("SELECT cities.id, cities.name, states.name \
+                    FROM cities, states WHERE states.id=cities.state_id \
                     ORDER BY cities.id ASC")
         table = cur.fetchall()
         for state in table:
