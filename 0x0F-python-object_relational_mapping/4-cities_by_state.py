1fc638c871e0ed2ef8593e658944b3da1dd77f0b
jarehec <bone1198@gmail.com>
2017-08-11 21:59:35 +0000

    added JOIN

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                              db=database, port=3306)
         cur = db.cursor()
         cur.execute("SELECT cities.id, cities.name, state.name \
-                    FROM cities, states WHERE states.id=cities.state_id \
+                    FROM cities JOIN states ON states.id=cities.state_id \
                     ORDER BY cities.id ASC")
         table = cur.fetchall()
         for state in table:
