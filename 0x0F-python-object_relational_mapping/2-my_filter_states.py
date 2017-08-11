66ff708cf609ab9647076337fc63952ed0699cf2
jarehec <bone1198@gmail.com>
2017-08-11 22:52:09 +0000

    fixed output

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,14 +5,13 @@
 if __name__ == "__main__":
     import MySQLdb
     import sys
-    if len(sys.argv) == 4:
-        db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                             passwd=sys.argv[2], db=sys.argv[3], port=3306)
-        cur = db.cursor()
-        cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-        table = cur.fetchall()
-        for state in table:
-            if state[1] == sys.argv[4]:
-                print(state)
-        cur.close()
-        db.close()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    data = cur.fetchall()
+    for state in data:
+        if state[1] == sys.argv[4]:
+            print(state)
+    cur.close()
+    db.close()
