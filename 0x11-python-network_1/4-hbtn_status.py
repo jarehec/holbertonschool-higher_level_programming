f26a0db089c16258e76cca2ff6373bf9f7c2a386
jarehec <bone1198@gmail.com>
2017-08-29 07:13:51 +0000

    replaced content with text method

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,6 +4,6 @@ from requests import get
 
 if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
-    response = get(url).content.decode('utf-8')
-    print("Body response:\n\t- type: {}\n\t- content {}"
+    response = get(url).text
+    print("Body response:\n\t- type: {}\n\t- content: {}"
           .format(type(response), response))
