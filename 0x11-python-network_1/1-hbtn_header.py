01f80a6a8152481dd9ffbc3097e6119f3b0ea432
jarehec <bone1198@gmail.com>
2017-08-29 07:46:48 +0000

    added with statement

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,5 +5,6 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    headers = request.urlopen(url).info()
-    print("{}".format(headers.get('X-Request-Id')))
+    with request.urlopen(url) as headers:
+        headers = headers.info()
+        print("{}".format(headers.get('X-Request-Id')))
