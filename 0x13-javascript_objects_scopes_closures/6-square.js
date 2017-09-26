7bc18c2a45769396e247dc08c07d9fdba7400d80
jarehec <bone1198@gmail.com>
2017-09-26 02:05:20 +0000

    inherit from square class

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 // square class that inherits from square
+const quare = require('./5-square').Square;
 exports.Square = function Square (size) {
+  quare.call(this, size);
   Square.prototype.charPrint = function (c = 'X') {
     for (let i = size; i > 0; i--) {
       console.log(c.repeat(size));
