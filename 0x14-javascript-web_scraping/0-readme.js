52faf1fbaa46ffd3f1061874df85abcf51c48580
jarehec <bone1198@gmail.com>
2017-09-26 20:08:29 +0000

    using process.stdout.write instead of console.log

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // reads and prints the content of a file
 let fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', function(err, data) {
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) throw err;
-  console.log(data);
+  process.stdout.write(data);
 });
