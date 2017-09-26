647177494bac7b22e4bcafad5e3375f9c6328872
jarehec <bone1198@gmail.com>
2017-09-26 21:44:19 +0000

    Print err

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,6 +2,5 @@
 // reads and prints the content of a file
 let fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
-  if (err) throw err;
-  process.stdout.write(data);
+  if (err) { console.log(err); } else { process.stdout.write(data); }
 });
