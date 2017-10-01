858145599dc6caa808d41f7b3243bdf827868dc7
jarehec <jarehec@users.noreply.github.com>
2017-10-01 15:54:59 -0700

    reduced line count

--- 0x12-javascript-warm_up/Rusty/5-to_integer.rs
+++ 0x12-javascript-warm_up/Rusty/5-to_integer.rs
@@ -1,22 +1,14 @@
-/* prints 'My number:' and the first arguemnt can be converted into an int */
+/* prints 'My number:' and the first arguemnt that can be converted into an int */
 use std::env;
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
 
+	print!("My number: ");
 	if args.len() > 1 {
-		match args[1].parse::<isize>() {
-			Ok(n) => {
-				println!("My number: {}", args[1]);
-				n
-			},
-			Err(_) => {
-				println!("My number: undefined");
-				-1
-			},
+		match args[1].parse::<usize>() {
+			Ok(n) => { println!("{}", args[1]); n },
+			Err(_) => { println!(""); 0 },
 		};
-	}
-	else {
-		println!("My number: undefined");
 	}
 }
